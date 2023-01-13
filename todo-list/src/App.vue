18f463539193f7e079517cc6f0fdf6f481cc4549
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2023-01-13 12:28:15 +0100

    Sviluppo grafica: -Inserita un'img come sfondo del container principale; - Creato un sotto container per dare all'immagine un effetto di opacity; - Creato un file general.scss per le regole grafiche principali; -importati due font da google;

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -49,55 +49,71 @@ export default {
 
 <template>
   <div class="container">
-    <h1>Todo List</h1>
+    <div class="sub_container">
+      <h1>ANIME DA GUARDARE</h1>
 
-    <div class="ctn_list">
-      <!-- form per inserire un nuovo elemento -->
-      <form @submit="createElem">
-        <label for="newElem">Inserisci un nuovo elemento: </label>
-        <input type="text" name="newElem" v-model="newElem">
-        <input type="submit" value="Crea">
-      </form>
-      <!-- lista in cui stampo i dati contenuti nel server -->
-      <ul>
-        <li v-for="(elem, ind) in todoList" :key="ind">
-          {{ elem.text }}
-        </li>
-      </ul>
+      <div class="ctn_list">
+        <!-- form per inserire un nuovo elemento -->
+        <form @submit="createElem">
+          <label for="newElem">Inserisci un nuovo elemento: </label>
+          <input type="text" name="newElem" v-model="newElem">
+          <input type="submit" value="Crea">
+        </form>
+        <!-- lista in cui stampo i dati contenuti nel server -->
+        <ul>
+          <li v-for="(elem, ind) in todoList" :key="ind">
+            {{ elem.text }}
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@use "./styles/general.scss" as *;
+
 .container {
-  background-color: blue;
-  width: 900px;
-  height: fit-content;
+  background-image: url("./assets/img/mixanime.jpg");
+  background-size: cover;
+  width: 1140px;
+  height: 800px;
   margin: 100px auto;
-  text-align: center;
 
-  h1 {
-    color: #ffffff;
-  }
-
-  .ctn_list {
-    color: #ffffff;
-    width: 80%;
-    margin: 0 auto;
+  .sub_container {
+    background-color: rgba($color: #000000, $alpha: 0.7);
+    width: 100%;
+    height: 100%;
     padding: 10px;
-    text-align: start;
+    text-align: center;
+    overflow-y: auto;
 
-    ul {
-      padding: 0;
-      list-style: none;
+    h1 {
+      color: #ffffff;
+      font-family: 'Martian Mono', monospace;
+      font-size: 40px;
+    }
 
-      li {
-        font-size: 20px;
-        border-bottom: solid 1px #ffffff;
-        padding-bottom: 10px;
-        margin-bottom: 20px;
+    .ctn_list {
+      color: #ffffff;
+      width: 80%;
+      margin: 0 auto;
+      padding: 10px;
+      text-align: start;
+
+      ul {
+        padding: 0;
+        list-style: none;
+
+        li {
+          font-size: 20px;
+          border-bottom: solid 1px #ffffff;
+          padding-bottom: 10px;
+          margin-bottom: 20px;
+        }
       }
     }
   }
+
 }
 </style>
